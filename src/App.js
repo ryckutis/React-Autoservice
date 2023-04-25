dff038914adb53e5b99e457309262762b90730dd
ryckutis <ricardasgr225@gmail.com>
2023-04-25 21:22:46 +0300

    created navbar with routes n shit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/UI/atoms/NavBar/NavBar';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" />
+        <Route path="/about" />
+        <Route path="/services">
+          <Route path="diagnostics" />
+          <Route path="batteries" />
+        </Route>
+        <Route path="/blog">
+          <Route path="blog1" />
+          <Route path="blog2" />
+        </Route>
+        <Route path="/contact" />
+      </Routes>
     </div>
   );
 }
