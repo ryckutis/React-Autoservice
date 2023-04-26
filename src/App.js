b64f2f9dca1d7fe76184093674503cb3e95ad9c4
ItzNico-dev <matas.venslauskas.mv@gmail.com>
2023-04-26 23:21:59 +0300

    Blog Single done

--- src/App.js
+++ src/App.js
@@ -2,24 +2,26 @@ import './App.css';
 import NavBar from './components/UI/atoms/NavBar/NavBar';
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './components/UI/organisms/HomePage/HomePage';
+import BlogSingle from './components/UI/molecules/BlogSingle/BlogSingle';
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" />
-        <Route path="/services">
-          <Route path="diagnostics" />
-          <Route path="batteries" />
+      {/* <NavBar /> */}
+      {/* <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' />
+        <Route path='/services'>
+          <Route path='diagnostics' />
+          <Route path='batteries' />
         </Route>
-        <Route path="/blog">
-          <Route path="blog1" />
-          <Route path="blog2" />
+        <Route path='/blog'>
+          <Route path='blog1' />
+          <Route path='blog2' />
         </Route>
-        <Route path="/contact" />
-      </Routes>
+        <Route path='/contact' />
+      </Routes> */}
+      <BlogSingle />
     </div>
   );
 }
