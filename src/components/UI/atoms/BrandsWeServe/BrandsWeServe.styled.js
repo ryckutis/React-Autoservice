8a1b7290e4ad4dbe22bb042ee85db225c338c190
ryckutis <ricardasgr225@gmail.com>
2023-04-26 19:43:37 +0300

    split component into atoms

--- src/components/UI/atoms/BrandsWeServe/BrandsWeServe.styled.js
+++ src/components/UI/atoms/BrandsWeServe/BrandsWeServe.styled.js
@@ -6,34 +6,6 @@ export const StyledMainDiv = styled.div`
   font-family: var(--primary-font-family);
 `;
 
-export const Wrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-wrap: wrap;
-  position: relative;
-  padding: 2.5em 0;
-
-  &:before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: -20vw;
-    width: 200vw;
-    height: 100%;
-    background-color: #f2f2f2;
-    z-index: -1;
-  }
-`;
-
-export const StyledTitle = styled.h2`
-  margin: 0;
-  font-weight: 800;
-  font-size: 58px;
-  line-height: 76px;
-  color: #661ce7;
-`;
-
 export const StyledPar = styled.p`
   margin: 0;
   font-weight: 600;
